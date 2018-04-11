5ed90d369bd36fa6a179f05f77a5a56cdea2fa31
GkHabib <h.ghadimi1998@gmail.com>
2018-04-12 01:53:12 +0430

    data memory added, write enable signal name of register memory file fixed

---
+++ ca2_memari/dataMemory.v
@@ -0,0 +1,19 @@
+module dataMemory(clk, rst, writeEn, address, readData, writeData);
+  input clk, rst, writeEn;
+  input[7:0] address, writeData;
+  output[7:0] readData;
+  reg[7:0] dataArray[255:0];
+  always@(posedge clk, posedge rst) begin
+    if(rst) begin
+      for (int i=0; i<=255; i=i+1) begin
+        dataArray[i] = 8'b0;
+      end
+    end
+    else begin
+      if(writeEn) begin
+        dataArray[address] <= writeData;
+      end
+    end
+  end
+  assign readData = dataArray[address];
+endmodule
--- ca2_memari/registerMemory.v
+++ ca2_memari/registerMemory.v
@@ -1,17 +1,17 @@
-module registerMemory(clk, rst, regWriteEn, readAdr1, readAdr2, writeAdr, readData1, readData2, writeData);
+module registerMemory(clk, rst, writeEn, readAdr1, readAdr2, writeAdr, readData1, readData2, writeData);
   input[3:0] readAdr1 , readAdr2, writeAdr;
   output[7:0] readData1, readData2;
   input[7:0] writeData;
   input clk, rst, regWriteEn;
   reg[8:0] registersMemory[4095:0];
-  always @ (posedge clk, posedge rst) begin
+  always@(posedge clk, posedge rst) begin
     if(rst) begin
       for (int i=0; i<=7; i=i+1) begin
         registersMemory[i] = 8'b0;
       end
     end
     else begin
-      if(regWriteEn) begin
+      if(writeEn) begin
         registerMemory[writeAdr] <= writeData;
       end
     end
